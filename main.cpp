6d55ded4d48a137256a4090a33e9fa5753748135
(TitovaVD) <lera.titova.2000@list.ru>
2019-05-26 14:18:47 +0300

    code:выделена функция для печати отдельного байта в двоичной системе

--- main.cpp
+++ main.cpp
@@ -8,6 +8,13 @@ char nibble_to_hex (uint8_t i)
     char symbols[17]="0123456789abcdef";
     return symbols[i];
 }
+char
+bit_digit(uint8_t byte, uint8_t bit) {
+    if (byte & (0x1 << bit)) {
+        return '1';
+    }
+    return '0';
+}
 int main()
 {
  cout<<nibble_to_hex (15);
